8b5164f1e4c02f081a8b3b20dcf50e54d92ed497
kylianvanalphen <kylian.van.alphen@outlook.be>
2020-04-21 12:29:44 +0200

    Added count loop

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ import time
 import RPi.GPIO as GPIO
 from lib import Database, MoistureSensor, TemperatureSensor
 
+# Setup GPIO stuff
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(23, GPIO.OUT)
@@ -11,10 +12,18 @@ database = Database("provil-ict.be", "gip_agritower", "agritower", "gip_2019_agr
 moisture_sensor = MoistureSensor(0)
 temperature_sensor = TemperatureSensor()
 
+# Main program
 def main():
+    count = 0
+
     while True:
-        database.insertMoisture(moisture_sensor.getMoisture())
-        database.insertTemperature(temperature_sensor.getTemperature())
+        count += 1
+
+        # Loop every second -> 10 = 10 seconds
+        if count == 10:
+            count = 0
+            database.insertMoisture(moisture_sensor.getMoisture())
+            database.insertTemperature(temperature_sensor.getTemperature())
 
         # if 0 <= m and m < 300:
         #     result = 'Dry'
@@ -23,26 +32,13 @@ def main():
         # else:
         #     result = 'Wet'
         # print('Moisture value: {0}, {1}'.format(m, result))
+
+        # Control LED1
+        led1 = database.selectOutput("LED1")
+        GPIO.output(23, int(led1[2]))
+        print "Set %s to %i" % (led1[1], led1[2])
         
-        # database.selectOutput("POMP")
-
-        # led1 = database.selectOutput("LED1")
-        # print "status of led 1 is " + str(led1[2])
-
-        # led2 = database.selectOutput("LED2")
-        # print "status of led 2 is " + str(led1[2])
-
-        # curs.execute("SELECT * FROM outputs ")
-        # result_set = curs.fetchall()
-        # for row in result_set:
-        #     print "ID: " + str(row[0])
-        #     print "Name: " + row[1]
-        #     print "Status: " + str(row[2])
-            
-        #     if row[1] == "LED":
-        #         GPIO.output(23, int(row[2]))
-        
-        time.sleep(10)
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
