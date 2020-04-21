d53f250a274d00595152ee1528d6a77c0e1721d1
kylianvanalphen <kylian.van.alphen@outlook.be>
2020-04-21 11:38:06 +0200

    Added basic MySQL functionality

--- lib/database.py
+++ lib/database.py
@@ -1,6 +1,31 @@
 import MySQLdb
-
-db = MySQLdb.connect("provil-ict.be","gip_agritower","agritower","gip_2019_agritower")
-curs=db.cursor()
+from datetime import datetime
 
 class Database:
+    def __init__(self, host, username, password, database):
+        self.db = MySQLdb.connect(host, username, password, database)
+        self.cursor = self.db.cursor()
+    
+    def insertMoisture(self, value):
+        current_dt = datetime.now()
+
+        try:
+            self.cursor.execute("INSERT INTO moistureSensor (datum, waarde) VALUES (%s,%s)", (current_dt.strftime("%Y/%m/%d %H:%M:%S"), str(value)))
+            self.db.commit()
+        except:
+            print "Error while inserting moisture sensor data"
+            self.db.rollback()
+
+    def insertTemperature(self, value):
+        current_dt = datetime.now()
+
+        try:
+            self.cursor.execute("INSERT INTO temperatureSensor (datum, waarde) VALUES (%s,%s)", (current_dt.strftime("%Y/%m/%d %H:%M:%S"), str(value)))
+            self.db.commit()
+        except:
+            print "Error while inserting temperature sensor data"
+            self.db.rollback()
+
+    def selectOutput(self, name):
+        curs.execute("SELECT * FROM outputs WHERE name = %s" % name)
+        result_set = curs.fetchone()
